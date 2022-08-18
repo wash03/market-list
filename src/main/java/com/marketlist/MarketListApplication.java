92cf1a70f41f635f044a2f0147a5063b038cbf10
Washington Silva <wstsilva.ws@gmail.com>
2022-08-18 19:26:06 -0300

    .

---
+++ src/main/java/com/marketlist/MarketListApplication.java
@@ -0,0 +1,13 @@
+package com.marketlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marketlist/MarketListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
